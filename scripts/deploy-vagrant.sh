60fa7b93862685c8afe7cc025598b8bb70f83928
githubfoam <lequestionis@gmail.com>
2020-06-21 11:55:01 +0300

    ста́рый чуда́к

--- scripts/deploy-vagrant.sh
+++ scripts/deploy-vagrant.sh
@@ -29,15 +29,21 @@ vagrant up --provider=libvirt "vg-opensuse-01"
 # https://app.vagrantup.com/suse/boxes/sles12sp2
 vagrant box add  "suse/sles12sp2" --provider=libvirt
 vagrant up --provider=libvirt "vg-sles-01"
-# # https://app.vagrantup.com/opensuse/boxes/Kubic.x86_64
-# vagrant box add  "opensuse/Kubic.x86_64" --provider=libvirt
-# vagrant up --provider=libvirt "vg-opensusekubicx8664"
-# # https://app.vagrantup.com/opensuse/boxes/Kubic.aarch64
-# vagrant box add  "opensuse/Kubic.aarch64" --provider=libvirt
-# vagrant up --provider=libvirt "vg-opensusekubicaarch64"
-# # https://app.vagrantup.com/opensuse/boxes/Kubic.aarch64
-# vagrant box add  "opensuse/Kubic.aarch64" --provider=libvirt
-# vagrant up --provider=libvirt "vg-opensusekubicaarch64"
+# https://app.vagrantup.com/opensuse/boxes/Kubic.x86_64
+vagrant box add  "opensuse/Kubic.x86_64" --provider=libvirt
+vagrant up --provider=libvirt "vg-opensusekubicx8664"
+# https://app.vagrantup.com/opensuse/boxes/Kubic.aarch64
+vagrant box add  "opensuse/Kubic.aarch64" --provider=libvirt
+vagrant up --provider=libvirt "vg-opensusekubicaarch64"
+# https://app.vagrantup.com/opensuse/boxes/MicroOS.x86_64
+vagrant box add  "opensuse/MicroOS.x86_64" --provider=libvirt
+vagrant up --provider=libvirt "vg-microosx8664"
+# https://app.vagrantup.com/opensuse/boxes/MicroOS.aarch64
+vagrant box add  "opensuse/MicroOS.aarch64" --provider=libvirt
+vagrant up --provider=libvirt "vg-microosaarch64"
+# https://app.vagrantup.com/opensuse/boxes/Tumbleweed.aarch64
+vagrant box add  "opensuse/Tumbleweed.aarch64" --provider=libvirt
+vagrant up --provider=libvirt "vg-tumbleweedaarch64"
 vagrant status
 virsh list --all #show all running KVM/libvirt VMs
 vagrant box list #veridy installed boxes
